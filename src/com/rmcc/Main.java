7cec865c8a5352bf63950efc336b744302999919
River McCloskey <rivermccloskey99@gmail.com>
2021-01-13 14:04:16 +0000

    Reads input, uses checkstring method and loop for validity

--- src/com/rmcc/Main.java
+++ src/com/rmcc/Main.java
@@ -1,8 +1,23 @@
 package com.rmcc;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Choose the verstion you would like to sync your saves from (se/vr): ");
+        String version = "";
+
+        while (checkString(version)) {
+            System.out.println("Choose the version you would like to sync your saves from (se/vr): ");
+            Scanner sc = new Scanner(System.in);
+            version = sc.nextLine();
+            if(checkString(version))
+                System.out.println("Invalid input, try again");
+        }
+        System.out.println("Version selected was: " + version);
+    }
+
+    public static boolean checkString(String version) {
+        return !version.equalsIgnoreCase("SE") && !version.equalsIgnoreCase("VR");
     }
 }
