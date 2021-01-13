066033433c32ccfea3255edf020d7e4da3603ac8
40202510 <rmccloskey17@qub.ac.uk>
2021-01-13 12:59:03 +0000

    Started project, first line to ask which version to sync from

---
+++ src/com/rmcc/Main.java
@@ -0,0 +1,8 @@
+package com.rmcc;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Choose the verstion you would like to sync your saves from (se/vr): ");
+    }
+}
