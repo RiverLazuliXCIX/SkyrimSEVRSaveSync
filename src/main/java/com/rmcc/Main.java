92f243aa1e9e0813f4820a3509be7541aad24a99
River McCloskey <rivermccloskey99@gmail.com>
2021-01-13 19:12:57 +0000

    Using FileUtils for the copying of the contents of one folder

--- src/main/java/com/rmcc/Main.java
+++ src/main/java/com/rmcc/Main.java
@@ -1,10 +1,17 @@
 package com.rmcc;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Scanner;
+import org.apache.commons.io.FileUtils;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String version = "";
 
         while (checkString(version)) {
@@ -15,6 +22,11 @@ public class Main {
                 System.out.println("Invalid input, try again");
         }
         System.out.println("Version selected was: " + version);
+
+        File src = new File("D:\\River\\Documents\\___SkyrimTest\\SE");
+        File dest = new File("D:\\River\\Documents\\___SkyrimTest\\VR");
+
+        FileUtils.copyDirectory(src, dest);
     }
 
     public static boolean checkString(String version) {
